9ae859bf4ff5316a7b474dc23f9d2bf88da19c29
bakon <bakon@onchaonapps.io>
2025-03-11 14:52:25 -0500

    ollama lib for browser

--- ai_agent_ui/src/renderer/src/components/PromptInputInterface/PromptInputInterface.tsx
+++ ai_agent_ui/src/renderer/src/components/PromptInputInterface/PromptInputInterface.tsx
@@ -3,7 +3,7 @@ import { Sheet, Input, Button, List, ListItem, Typography } from '@mui/joy'
 import SendIcon from '@mui/icons-material/Send'
 import ClearIcon from '@mui/icons-material/Clear'
 import { useModel } from '../../hooks/useModel'
-import { Ollama } from 'ollama'
+import Ollama from 'ollama/browser';
 import { useAIEndpoint } from '../../hooks/useEndpointHook'
 import { SelectOllamaModel } from '../../components/SelectModelComponent/SelectModelComponent'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
